43b6341e9ae396ec9d4fd92d6f83619f632db1b1
manoldzhermanski <64347529+manoldzhermanski@users.noreply.github.com>
2020-07-29 11:30:43 +0300

    Update and rename Planet.cpp to Structures/Planet/Planet.cpp

--- Structures/Planet/Planet.cpp
+++ Structures/Planet/Planet.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 
 int main() {
-	
+	Planet p;
+	p.init();
+	p.print();
+	std::cout << neededSeconds(p) << std::endl;
+
 	Planet test[2];
 	createArray(test,2);
 	printArray(test,2);
-	std::cout<< biggestDiameter(test, 2);
+	std::cout<< biggestDiameter(test, 2) << std::endl;
 	
 	return 0;
 }
